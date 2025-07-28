773137b2747c99f336db26c95b8e738b104af47a
isatyamks <isatyamks@gmail.com>
2025-07-28 18:55:57 +0530

    chore: initialize frontend project with package.json and dependencies

---
+++ frontend/next.config.ts
@@ -0,0 +1,27 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  images: {
+    domains: [
+      
+      'images.unsplash.com',
+      'randomuser.me', 
+      'upload.wikimedia.org',
+      'cf.bstatic.com',
+      'cache.marriott.com',
+      'media-cdn.tripadvisor.com',
+      'r2imghtlak.mmtcdn.com',
+      'i.guim.co.uk',
+      'media1.thrillophilia.com',
+      'dynamic-media-cdn.tripadvisor.com',
+      'cdn.britannica.com',
+      'static.wixstatic.com',
+      'img.taste.com.au',
+      'www.thespruceeats.com',
+      'deliciousmemorieswithalves.wordpress.com'
+    ],
+  },
+  /* config options here */
+};
+
+export default nextConfig;
